ac734db335e78eb719c1f3cc09a4d65eccd71776
Ham3798 <5023798@naver.com>
2025-11-15 14:51:57 +0900

    remove: legacy files

--- ccip-faucet-fe/src/lib/config/core/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Core Configuration Exports
- */
-
-export * from './types'
-export * from './loader'
-
-// Re-export the singleton instance for convenience
-export { configLoader } from './loader' 
